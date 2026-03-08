6a34c0ef33d287c44190d3138dd1a9fd07bc001a
Michiel Bruins <1969831+mrbruins@users.noreply.github.com>
2026-03-08 13:41:43 +0100

    Add Determinate Nix configuration and update flake inputs

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -11,20 +11,15 @@ let user = "michielbruins"; in
      agenix.darwinModules.default
   ];
 
+  # Let Determinate Nix manage the Nix installation when present
+  determinateNix.enable = true;
+
   # Setup user, packages, programs
   nix = {
-    package = pkgs.nix;
-
     settings = {
       trusted-users = [ "@admin" "${user}" ];
       substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-    };
-
-    gc = {
-      automatic = true;
-      interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 30d";
     };
 
     extraOptions = ''
